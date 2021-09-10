c12befd99e8f58e9942df3968b5cad8065bdc163
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-10 17:32:22 +0300

    fixed keys

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
  
 export MOVIE_API_KEY=7ddc7c8a6a7b5f95beefced38dc43326
-export SECRET_KEY=ead4ba544d5d4f75942062f71d3b4720
+export SECRET_KEY='b\xc4<\xaa\xf7S\x87\xa3\x88&\x89\xb0\xfb\xce\xa8\x00\x03\x80u!t/\x7f\x88'
+
+python3.8 manage.py server
