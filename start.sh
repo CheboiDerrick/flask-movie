2f46b436639c16897d0c5578053ccb09346a1e95
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-10 19:02:06 +0300

    fix config

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
  
-export MOVIE_API_KEY=7ddc7c8a6a7b5f95beefced38dc43326
-export SECRET_KEY='b\xc4<\xaa\xf7S\x87\xa3\x88&\x89\xb0\xfb\xce\xa8\x00\x03\x80u!t/\x7f\x88'
+export MOVIE_API_KEY='7ddc7c8a6a7b5f95beefced38dc43326'
+export SECRET_KEY='SECRET_KEY'
 
 python3.8 manage.py server
