c2c26ad63f84dad512d7cabe39a3e15a87ce6b49
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-19 17:01:58 +0300

    final state before deployment

--- config.py
+++ config.py
@@ -8,6 +8,23 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:#Mjamaica101!@localhost/watchlist'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
     @staticmethod
     def init_app(app):
         pass
@@ -31,9 +48,17 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:#Mjamaica101!@localhost/watchlist'
+
     DEBUG = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:#Mjamaica101!/watchlist_test'
+
+
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
- 
 export MOVIE_API_KEY='7ddc7c8a6a7b5f95beefced38dc43326'
 export SECRET_KEY='SECRET_KEY'
+
+export MAIL_USERNAME='derrick.cheboi@student.moringaschool.com'
+export MAIL_PASSWORD='olqmzquawfyerqdo'
 
 python3.8 manage.py server
